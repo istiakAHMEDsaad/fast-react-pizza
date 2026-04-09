ac45e6f1ab346dde15dc9c779d086fc29b5339aa
istiakAHMEDsaad <istik2020@gmail.com>
2026-04-09 23:07:07 +0600

    nice loader

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -24,7 +24,7 @@ function CreateUser() {
 
       {username !== '' && (
         <div>
-          <Button>Start ordering</Button>
+          <Button to='/order/new'>Start ordering</Button>
         </div>
       )}
     </form>
