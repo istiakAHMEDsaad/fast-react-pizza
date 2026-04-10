cf724df99fec592bbf958454a0fc8582578a3b31
istiakAHMEDsaad <istik2020@gmail.com>
2026-04-10 23:00:22 +0600

    dispatch add cart

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 
-function Button({ disabled, children, to, type }) {
+function Button({ disabled, children, to, type, onClick }) {
   const base =
     'inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed cursor-pointer ';
 
@@ -12,12 +12,21 @@ function Button({ disabled, children, to, type }) {
       'inline-block text-sm rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5 cursor-pointer',
   };
 
-  if (to)
+  if (to) {
     return (
       <Link className={style[type]} to={to}>
         {children}
       </Link>
     );
+  }
+
+  if (onClick) {
+    return (
+      <button onClick={onClick} disabled={disabled} className={style[type]}>
+        {children}
+      </button>
+    );
+  }
 
   return (
     <button disabled={disabled} className={style[type]}>
@@ -31,6 +40,7 @@ Button.propTypes = {
   children: PropTypes.node,
   to: PropTypes.string,
   type: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 export default Button;
