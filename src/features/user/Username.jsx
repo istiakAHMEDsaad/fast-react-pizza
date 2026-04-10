ff0cb2409dc949cbb5abaec1a036e92f4f6b20bd
istiakAHMEDsaad <istik2020@gmail.com>
2026-04-10 17:24:49 +0600

    store provider

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,10 @@
+import { useSelector } from 'react-redux';
+
 function Username() {
-  return <p className="hidden text-sm font-semibold md:block">name</p>;
+  const username = useSelector((state) => state.user.username);
+  if (!username) return null;
+
+  return <p className="hidden text-sm font-semibold md:block">{username}</p>;
 }
 
 export default Username;
