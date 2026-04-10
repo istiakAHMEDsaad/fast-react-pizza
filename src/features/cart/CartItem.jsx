d92b553e7ff44137dac7701450a98f75ac1b370b
istiakAHMEDsaad <istik2020@gmail.com>
2026-04-10 12:49:20 +0600

    button element

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,16 +1,28 @@
+import Button from '../../ui/Button';
+import { formatCurrency } from '../../utils/helpers';
+import PropTypes from 'prop-types';
+
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
 
   return (
-    <li>
-      <p>
+    <li className="py-3 sm:flex sm:items-center sm:justify-between">
+      <p className="mb-1 sm:mb-0">
         {quantity}&times; {name}
       </p>
-      <div>
-        <p>{formatCurrency(totalPrice)}</p>
+      <div className="flex items-center justify-between sm:gap-6">
+        <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+        <Button type="small">Delete</Button>
       </div>
     </li>
   );
 }
+
+CartItem.propTypes = {
+  pizzaId: PropTypes.number,
+  name: PropTypes.string,
+  quantity: PropTypes.number,
+  totalPrice: PropTypes.number,
+};
 
 export default CartItem;
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,15 +2,14 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 
 function Button({ disabled, children, to, type }) {
-  // const className =
-  //   'inline-block cursor-pointer rounded-full bg-yellow-400 px-4 py-3 text-sm font-semibold tracking-wide text-stone-800 uppercase transition-colors hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed sm:px-6 sm:py-4';
-
   const base =
-    'inline-block cursor-pointer rounded-full bg-yellow-400 text-sm font-semibold tracking-wide text-stone-800 uppercase transition-colors hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed ';
+    'inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed cursor-pointer ';
 
   const style = {
-    primary: base + 'px-4 py-3 md:px-6 md:py-3',
-    small: base + 'px-4 py-2 md:px-5 md:py-2.5 text-xs',
+    primary: base + 'px-4 py-3 md:px-6 md:py-4',
+    small: 'px-4 py-2 md:px-5 md:py-2.5 text-xs',
+    secondary:
+      'inline-block text-sm rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5 cursor-pointer',
   };
 
   if (to)
